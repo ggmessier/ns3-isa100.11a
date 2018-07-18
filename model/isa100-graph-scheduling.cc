63e209f8dbc33bfcfcf8c176a2cc4dcacc508945
Rajith Karunaratne <rajith3k@gmail.com>
2018-07-18 02:08:55 -0600

    Scheduling update
    Required format modifications

--- model/isa100-graph-scheduling.cc
+++ model/isa100-graph-scheduling.cc
@@ -19,7 +19,7 @@
  */
 
 #include "isa100-graph-scheduling.h"
-//#include "isa-graph.h"
+#include "isa-graph.h"
 #include "ns3/log.h"
 #include "ns3/type-id.h"
 #include <algorithm>
@@ -29,11 +29,6 @@ NS_LOG_COMPONENT_DEFINE ("Isa100GraphScheduling");
 using namespace std;
 
 namespace ns3 {
-
-//bool CompareFrameRate ( map <uint32_t, int8_t> e1,  map <uint32_t, int8_t> e2)
-//{
-//  return (e1. < e2.m_powerOfrate);
-//};
 
 TypeId Isa100GraphScheduling::GetTypeId (void)
 {
@@ -47,29 +42,229 @@ TypeId Isa100GraphScheduling::GetTypeId (void)
 Isa100GraphScheduling::Isa100GraphScheduling ()
 {
   NS_LOG_FUNCTION (this);
-//  (this)->m_supperFrame = -2;
+  m_maximumChannelOffsets = 4;
 }
-//
-//Isa100GraphScheduling::Isa100GraphScheduling (Ptr<IsaGraph> G)
-//{
-//  NS_LOG_FUNCTION (this);
-//  (this)->m_supperFrame = -2;
-//  map <uint32_t, GraphNode> graphNodeMap = G->GetGraphNodeMap();
-//
-//  // powerOfrateMap is a map with power of sample rate (n of 2^n) to nodes (node Ids) with that sample rate
-//  map <uint32_t, vector<uint32_t>> powerOfrateMap;
-//
-//  for (map<uint32_t, GraphNode>::const_iterator it = graphNodeMap.begin ();
-//        it != graphNodeMap.end (); ++it)
-//    {
-//      int8_t tempPowerofrate = it->second.m_powerOfrate;
-//      powerOfrateMap[tempPowerofrate].push_back(it->first);
-//    }
-//}
 
 Isa100GraphScheduling::~Isa100GraphScheduling ()
 {
   NS_LOG_FUNCTION (this);
+}
+
+bool Isa100GraphScheduling::ConstructDataCommunicationSchedule (Ptr<IsaGraph> G, Ptr<IsaGraph> GUL,
+                                                                Ptr<IsaGraph> GB, map <uint32_t, Ptr<IsaGraph>> GDL)
+{
+  NS_LOG_FUNCTION (this);
+  map <uint32_t, GraphNode> graphNodeMap = G->GetGraphNodeMap();
+  Ptr<Node> gateway = G->GetGateway();        ///< g node of the algorithm
+
+  for (map<uint32_t, GraphNode>::const_iterator it = graphNodeMap.begin ();
+        it != graphNodeMap.end (); ++it)
+    {
+      // Identify the set of nodes with each sample rate: N 1 , N 2 , . . . , N k .
+      int8_t tempPowerofrate = it->second.m_powerOfrate;
+      // Sort device sample rates in ascending order: r 1 < r 2 < . . . < r k . (Here Map is storing data in sorted manner.)
+      (this)->m_groupSameSampleRate[tempPowerofrate].push_back(it->second.m_head);
+    }
+
+  vector<Ptr<Node>> GroupwithSampleRate;
+
+  // for all r i from r 1 to r k do
+  for (map<int8_t, vector<Ptr<Node>>>::const_iterator it = (this)->m_groupSameSampleRate.begin ();
+        it != (this)->m_groupSameSampleRate.end (); ++it)
+    {
+      GroupwithSampleRate = it->second;
+      // Generate the data Superframe F i
+      uint16_t superframe = (this)->GetTimeSlots(it->first);
+
+      // for all node v ∈ N i do
+      while(!GroupwithSampleRate.empty())
+        {
+          Ptr<Node> v = GroupwithSampleRate.back();
+          if(v != gateway)
+            {
+              // Schedule primary and retry links for publishing data
+              (this)->ScheduleLinks(v, gateway, GUL , superframe, 0, SLOT_EXCLUSIVE);
+              (this)->ScheduleLinks(v, gateway, GUL , superframe, superframe/4, SLOT_SHARED);
+              // Schedule primary and retry links for control data
+              (this)->ScheduleLinks(gateway, v, GDL[v->GetId()] , superframe, superframe/2, SLOT_EXCLUSIVE);
+              (this)->ScheduleLinks(gateway, v, GDL[v->GetId()] , superframe, superframe/4*3, SLOT_SHARED);
+            }
+          GroupwithSampleRate.pop_back();
+        }
+    }
+
+  return true;
+}
+
+// ScheduleLinks(u, v, G, F , t, o) - additionally channel offset has included.
+bool Isa100GraphScheduling::ScheduleLinks (Ptr<Node> u, Ptr<Node> v, Ptr<IsaGraph> Graph, uint32_t superframe,
+                                           uint16_t timeSlot, LinkOption option)
+{
+  NS_LOG_FUNCTION (this);
+  //Identify data superframe F ′ with l F ′ = 2l F
+  uint32_t superframeF_1 = superframe*2;
+  vector <Ptr<Node>> successorsOfU = Graph->GetGraphNode(u->GetId()).m_neighbors;
+  TimeScheduleStruct scheduleStructForU;          // schedule information for node u
+  TimeScheduleStruct scheduleStructForNext;          // schedule information for node v
+
+  scheduleStructForU.m_graphID = Graph;
+  scheduleStructForU.m_linkType = TX;
+
+  Slot slot;
+
+//  multimap<pair<Ptr<Node>, Slot>, TimeScheduleStruct> m_nodeSchedule;    ///< schedule for each node of the network
+//  multimap<Slot, pair<Ptr<Node>,TimeScheduleStruct>> m_schedule;     ///< Total schedule of the network
+
+  //  for all node i ∈ Successor(u) do
+  for (vector<Ptr<Node>>::const_iterator it = successorsOfU.begin ();
+        it != successorsOfU (); ++it)
+    {
+      Ptr<Node> next = successorsOfU.back();    // node i
+
+      slot = (this)->GetNextAvailableSlot(timeSlot, option);
+      uint16_t tempEarliestTime = slot.m_timeSlot;
+
+      // update the total schedule
+      pair<Ptr<Node>,TimeScheduleStruct> timeSchedulePair;
+      timeSchedulePair.first = u;
+      scheduleStructForU.m_nextNode = next;
+      scheduleStructForU.m_repLength = superframe;
+
+      pair<Ptr<Node>, Slot> tempNodeAndSlot;
+      map<Slot,TimeScheduleStruct> tempNodeSchedule;
+
+      // update the schedule of node Next
+      scheduleStructForNext = scheduleStructForU;
+      scheduleStructForNext.m_linkType = RX;
+      scheduleStructForNext.m_nextNode = u;
+
+      if (successorsOfU.size()==1)              // if i is the only successor of u then
+        {
+          // update the total schedule
+          timeSchedulePair.second = scheduleStructForU;
+          (this)->m_schedule[slot] = timeSchedulePair;
+
+          // update the schedule of node u
+          tempNodeAndSlot.first = u;
+          tempNodeAndSlot.second = slot;
+          (this)->m_nodeSchedule[tempNodeAndSlot] = scheduleStructForU;
+
+          // update the schedule of node Next
+          tempNodeAndSlot.first = next;
+          tempNodeAndSlot.second = slot;
+          (this)->m_nodeSchedule[tempNodeAndSlot] = scheduleStructForNext;
+
+          if (next != v)
+            {
+              (this)->ScheduleLinks(next, v, Graph , superframe, tempEarliestTime, option);
+            }
+        }
+      else
+        {
+          if(it==successorsOfU.begin ())
+            {
+              // update the total schedule
+              scheduleStructForU.m_repLength = superframeF_1;
+              timeSchedulePair.second = scheduleStructForU;
+              (this)->m_schedule[slot] = timeSchedulePair;
+
+              // update the schedule of node u
+              tempNodeAndSlot.first = u;
+              tempNodeAndSlot.second = slot;
+              (this)->m_nodeSchedule[tempNodeAndSlot] = scheduleStructForU;
+
+              // update the schedule of node Next
+              tempNodeAndSlot.first = next;
+              tempNodeAndSlot.second = slot;
+              scheduleStructForNext.m_repLength = superframeF_1;
+              (this)->m_nodeSchedule[tempNodeAndSlot] = scheduleStructForNext;
+            }
+          else
+            {
+              slot.m_timeSlot = tempEarliestTime + superframe;
+
+              // update the total schedule
+              scheduleStructForU.m_repLength = superframeF_1;
+              timeSchedulePair.second = scheduleStructForU;
+              (this)->m_schedule[slot] = timeSchedulePair;
+
+              // update the schedule of node u
+              tempNodeAndSlot.first = u;
+              tempNodeAndSlot.second = slot;
+              (this)->m_nodeSchedule[tempNodeAndSlot] = scheduleStructForU;
+
+              // update the schedule of node Next
+              tempNodeAndSlot.first = next;
+              tempNodeAndSlot.second = slot;
+              scheduleStructForNext.m_repLength = superframeF_1;
+              (this)->m_nodeSchedule[tempNodeAndSlot] = scheduleStructForNext;
+            }
+
+          if (next != v)
+            {
+              (this)->ScheduleLinks(next, v, Graph , superframeF_1, tempEarliestTime, option);
+            }
+        }
+    }
+
+
+  return true;
+}
+
+uint16_t Isa100GraphScheduling::GetTimeSlots (int8_t PowerOfrate)
+{
+  NS_LOG_FUNCTION (this);
+  return 2^PowerOfrate*100;
+}
+
+Slot Isa100GraphScheduling::GetNextAvailableSlot(uint16_t timeSlot, LinkOption option)
+{
+  NS_LOG_FUNCTION (this);
+  Slot slot;
+  slot.m_timeSlot = timeSlot;
+  slot.m_channelOffset = 0;
+  int8_t tempOffset = 0;
+
+  switch (option)
+  {
+    case SLOT_EXCLUSIVE:
+      while (tempOffset < (this)->m_maximumChannelOffsets)
+        {
+          if ((this)->m_schedule.count(slot) == 0)
+            {
+              return slot;
+            }
+          tempOffset++;
+          if (tempOffset == (this)->m_maximumChannelOffsets)
+            {
+              tempOffset = 0;
+              timeSlot++;
+            }
+          slot.m_channelOffset = tempOffset;
+          slot.m_timeSlot = timeSlot;
+        }
+      break;
+
+    case SLOT_SHARED:
+      while (tempOffset < (this)->m_maximumChannelOffsets)
+        {
+          if ((this)->m_schedule.count(slot) < 5)
+            {
+              return slot;
+            }
+          tempOffset++;
+          if (tempOffset == (this)->m_maximumChannelOffsets)
+            {
+              tempOffset = 0;
+              timeSlot++;
+            }
+          slot.m_channelOffset = tempOffset;
+          slot.m_timeSlot = timeSlot;
+        }
+      break;
+  }
+
+  return slot;
 }
 
 } // namespace ns3
--- model/isa100-graph-scheduling.h
+++ model/isa100-graph-scheduling.h
@@ -24,7 +24,7 @@
 
 #include "ns3/core-module.h"
 #include "ns3/isa100-11a-module.h"
-//#include "isa-graph.h"
+#include "isa-graph.h"
 #include <algorithm>
 
 using namespace std;
@@ -37,40 +37,51 @@ namespace ns3 {
  *
  */
 
+/** Slot information including the time slot and the channel offset value
+ *
+ */
+typedef struct
+{
+  uint16_t m_timeSlot;            ///< Scheduled slot number of the superframe
+  int8_t m_channelOffset;         ///< channel offset when channel hopping is available
+} Slot;
+
+/** Link schedule type; whether transmitting or recieving
+ *
+ */
+typedef enum
+{
+  TX,            ///< if the node transmitting the packet
+  RX             ///< if the node receiving the packet
+} LinkType;
+
+/** Indicates the result of attempting to schedule a routing algorithm solution.
+ *
+ */
+typedef enum
+{
+  SLOT_EXCLUSIVE,
+  SLOT_SHARED
+} LinkOption;         // temporary enumeration; DlLinkType of isa100-dl.h can be used during integration
+
 /** Used by Scheduling algorithm to create the packet schedule.
  *
-// */
-//typedef struct
-//{
-//  uint16_t m_source;              ///< Source node of the packet.
-//  uint16_t m_destination;         ///< Destination node of the packet.
-//  vector<uint16_t> m_slotSched;   ///< Array of slot numbers where the node is active
-//  vector<DlLinkType> m_slotType;  ///< What the node is actually doing in those slots (tx,rx,etc)
-//  uint16_t m_hopCount;                 ///< Number of hops to sink
+ */
+typedef struct
+{
+  uint16_t m_nextNode;              ///< Transmit node of the packet.
+  LinkType m_linkType;              ///< whether packet is transmitting or receiving
+//  Slot m_slot;                    ///< Schedule slot information including time slot and the channel offset
+  uint16_t m_repLength;             //< packet scheduled repetition length (depends on superframe size)
+  /** m_graphID
+   * 0x0000 - Uplink graph (0)
+   * 0x0001 - Downlink graph of node id = 1 (1)
+   * 0xFFFF - Broadcast graph (65535)
+   */
+  Ptr<IsaGraph> m_graphID;          ///< an ID to identify the graph (pointer to the graph)
+  LinkOption m_option;              ///< link option whether share or exclusive
 //  double m_pwr;                   ///< Transmit power required to reach the next hop.
-//  uint32_t m_totalPackets;             ///< Number of packets the node has to send.
-//} ScheduleStructNew;
-//
-///** Used to contain the superframe schedule for a node.
-// *
-// */
-//typedef struct
-//{
-//  Ptr<Node> m_head;               ///< pointer to the node of the network scheduled
-//  vector<uint16_t> m_slotSched;   ///< Array of slot numbers where the node is active
-//	vector<DlLinkType> m_slotType;  ///< What the node is actually doing in those slots
-//} NodeScheduleNew;
-//
-///** Indicates the result of attempting to schedule a routing algorithm solution.
-// *
-// */
-//typedef enum
-//{
-//	SCHEDULE_FOUND_N,
-//	INSUFFICIENT_SLOTS_N,
-//	NO_ROUTE_N,
-//	STARVED_NODE_N
-//} SchedulingResultNew;
+} TimeScheduleStruct;
 
 class Isa100GraphScheduling;
 
@@ -84,12 +95,42 @@ public:
 
   Isa100GraphScheduling ();
 
-//  Isa100GraphScheduling (Ptr<IsaGraph> G);
-
   virtual ~Isa100GraphScheduling ();
 
+  /** Construct the data communication schedule of the graph network
+   *
+   * @param G Pointer for the graph network that need to create the schedule
+   */
+  bool ConstructDataCommunicationSchedule (Ptr<IsaGraph> G, Ptr<IsaGraph> GUL, Ptr<IsaGraph> GB, map <uint32_t, Ptr<IsaGraph>> GDL);
+
+  /** Schedule link for specific graph type
+   *
+   * @param u and v are the source and destination of the communication
+   * @param Graph Pointer for the graph network that need to create the schedule
+   * @param superframe superframe size considering the sample rate
+   * @param timeSlot time slot that required to consider to find the next available slot
+   * @param option link option whether exclusive or shared
+   */
+  bool ScheduleLinks (Ptr<Node> u, Ptr<Node> v, Ptr<IsaGraph> Graph, uint32_t superframe, uint16_t timeSlot, LinkOption option);
+
+  /** Get the total time slots for a given power of sample rate value
+   * TS = 10ms; Total time slots = (2^PowerOfrate)*100 // [sample rate in ms *1000/10]
+   *
+   * @param PowerOfrate power of the sample rate (n of 2^n): range is -2 to 9
+   */
+  uint16_t GetTimeSlots (int8_t PowerOfrate);
+
+  /** Get next available channel offset and the time slot
+   *
+   * @param timeSlot time slot that required to consider to find the next available slot
+   */
+  Slot GetNextAvailableSlot(uint16_t timeSlot, LinkOption option);
+
 private:
-//  uint32_t m_supperFrame;        ///< Supper Frame for all the nodes in the network
+  int8_t m_maximumChannelOffsets;                               ///< maximum available channel offsets for the network. ***this need to be set in helper class***
+  multimap<pair<Ptr<Node>, Slot>, TimeScheduleStruct> m_nodeSchedule;    ///< schedule for each node of the network
+  multimap<Slot, pair<Ptr<Node>,TimeScheduleStruct>> m_schedule;     ///< Total schedule of the network
+  map<int8_t, vector<Ptr<Node>>> m_groupSameSampleRate;         ///< group all the nodes with same sample rate
 
 };
 
